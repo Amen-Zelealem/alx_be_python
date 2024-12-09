9ef4929ca9ee8268892f5e74546636b066188d15
Amen Zelealem <amenzelealemtadese@gmail.com>
2024-12-09 18:26:23 +0300

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Assign specific values to the variables
+principal = 1000  # Representing $1000
+rate = 0.05  # Representing 5% Annual Interest
+time = 3  # Representing 3 Years
+
+# Calculate the Simple interst using the Formula (I = P * R * T)
+interset = principal * rate * time
+
+#  Print the Sample interst
+print(f"The simple interest is: {interset} ")
